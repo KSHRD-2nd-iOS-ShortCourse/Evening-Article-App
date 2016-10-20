d609323408920275f6048a5742c09b97d6556015
Kokpheng <kokpheng@kokphengs-mac.local>
2016-10-20 13:41:46 +0700

    Initial Commit

---
+++ Article App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Article App
+//
+//  Created by Kokpheng on 10/20/16.
+//  Copyright © 2016 Kokpheng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
